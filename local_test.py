cba41acdef7fc4a620b98666ecb9c51277ecc919
sche34 <timo@scheidel.nl>
2024-10-14 17:14:46 +0200

    no double env calling & better logging

--- local_test.py
+++ local_test.py
@@ -1,6 +1,7 @@
 import psycopg2
 import dotenv
 import os
+import time
 
 dotenv.load_dotenv()
 
@@ -9,13 +10,11 @@ user=os.getenv("POSTGRES_USER")
 password=os.getenv("POSTGRES_PASSWORD")
 host=os.getenv("POSTGRES_HOST")
 
-connection = psycopg2.connect(database=os.getenv("POSTGRES_DB"),
-                            user=os.getenv("POSTGRES_USER"),
-                            password=os.getenv("POSTGRES_PASSWORD"),
-                            host=os.getenv("POSTGRES_HOST"),
+connection = psycopg2.connect(database=database,
+                            user=user,
+                            password=password,
+                            host=host,
                             port=5432)
-
-
 
 cursor = connection.cursor()
 
@@ -25,8 +24,7 @@ select
 from 
     test_table2
 """
-
-
+start_time = time.time()
 cursor.execute(sql_context)
 
 # Fetch all rows from database
@@ -35,4 +33,8 @@ record = cursor.fetchall()
 connection.close()
 cursor.close()
 
-print("Last row from Database:- ", record[-1])
+if len(record) > 100:
+    print(f"Last row out of total {len(record)} rows:- {record[-1]}")
+else:
+    print(f'All {len(record)} row(s): {record}')
+print(f'Executed query in {time.time()-start_time:.0f} seconds')
