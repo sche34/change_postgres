9c0e37cb6f328c056806395e83c2db5a09c8c1a6
timoscheidel <timo@scheidel.nl>
2024-10-21 10:51:55 +0200

    improved readability

--- local_test.py
+++ local_test.py
@@ -1,3 +1,6 @@
+# this file is used to test the connection to the database. 
+# It is not used in the main code, but it is useful to check if the connection to the database is working properly.
+
 import psycopg2
 import dotenv
 import os
@@ -5,34 +8,32 @@ import time
 
 dotenv.load_dotenv()
 
-database=os.getenv("POSTGRES_DB")
-user=os.getenv("POSTGRES_USER")
-password=os.getenv("POSTGRES_PASSWORD")
-host=os.getenv("POSTGRES_HOST")
-
-connection = psycopg2.connect(database=database,
-                            user=user,
-                            password=password,
-                            host=host,
+# Connect to the database 
+connection = psycopg2.connect(database=os.getenv("POSTGRES_DB"),
+                            user=os.getenv("POSTGRES_USER"),
+                            password=os.getenv("POSTGRES_PASSWORD"),
+                            host=os.getenv("POSTGRES_HOST"),
                             port=5432)
 
 cursor = connection.cursor()
 
-sql_context ="""
+sql_query ="""
 select 
     *
 from 
     test_table2
 """
-start_time = time.time()
-cursor.execute(sql_context)
 
 # Fetch all rows from database
+start_time = time.time()
+cursor.execute(sql_query)
 record = cursor.fetchall()
 
+# Close the connection
 connection.close()
 cursor.close()
 
+# Print the last row if the total number of rows is greater than 100, otherwise print all rows
 if len(record) > 100:
     print(f"Last row out of total {len(record)} rows:- {record[-1]}")
 else:
